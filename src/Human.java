22daabd4869719ba8890b7a69d516add33fa8c22
GronkowskiL <lukasz.gronkowski99@gmail.com>
2023-02-03 12:57:22 +0100

    3/4

--- src/Human.java
+++ src/Human.java
@@ -1,5 +1,8 @@
+import java.util.Date;
 
 public class Human {
+    private Double wynagrodzenie;
+
     String firstName;
     String lastName;
     int yearOfBirth;
@@ -23,5 +26,22 @@ public class Human {
 
     public void setDevice(Car device) {
         this.device = device;
+    }
+
+    public Double getWynagrodzenie() {
+        System.out.println("Pobrano informacje o wynagrodzeniu " + new Date());
+        System.out.println("Wartość wynagrodzenia wynosi: " + wynagrodzenie);
+        return wynagrodzenie;
+    }
+
+    public void setWynagrodzenie(Double noweWynagrodzenie) {
+        if (noweWynagrodzenie < 0) {
+            System.out.println("Nie można przypisać ujemnej wartości wynagrodzenia!");
+            return;
+        }
+        System.out.println("Wysłano nowe dane o wynagrodzeniu do systemu księgowego");
+        System.out.println("Konieczne jest odebranie aneksu do umowy od pani Hani z kadr");
+        System.out.println("ZUS i US już wiedzą o zmianie wynagrodzenia, nie ma sensu ukrywać dochodu");
+        this.wynagrodzenie = noweWynagrodzenie;
     }
 }
